82af9296bf1ca25b57af97f6df4449ca76d1f901
Kendall González <kendall.webs.tl@gmail.com>
2018-03-03 17:51:02 -0600

    Exercise 5 finished

---
+++ Homework1/Exercise5.m
@@ -0,0 +1,25 @@
+1;
+
+% Tecnologico de Costa Rica
+% Escuela de Ingenieria Electronica
+% El-5805 Procesamiento Digital de Señales
+
+% This Octave File solves the
+% problems given for the Course
+% Digital Signal Processing
+
+% Algorithm explained in PDF attached
+function create = play(f,t)
+  bitrate = 44100
+  
+  domain = 0:bitrate*t;
+  
+  frequency = f/bitrate;
+  
+  y = sin(2*pi*frequency*domain);
+  
+  sound(y,bitrate)  
+  
+end
+
+play(7000,1)
